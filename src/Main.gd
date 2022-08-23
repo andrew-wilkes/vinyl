acbb57ef58f4e6d53138fd5046bfe3262e1b8068
andrew-wilkes <github@urgentclick.com>
2022-08-23 09:08:09 +0900

    Add save/load code to global

--- src/Main.gd
+++ src/Main.gd
@@ -4,20 +4,3 @@ var settings
 
 func _ready():
 	pass
-
-
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.scancode == KEY_ESCAPE:
-			save_and_quit()
-
-
-# Handle shutdown of App
-func _notification(what):
-	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
-		save_and_quit()
-
-
-func save_and_quit():
-	g.settings.save_data()
-	get_tree().quit()
