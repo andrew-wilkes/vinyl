aecc7bbefa6f8b192038ea400da3d207ad491da9
andrew-wilkes <github@urgentclick.com>
2022-10-10 15:40:44 +0900

    Add shader for sleeve

--- src/scenes/Sleeve.gd
+++ src/scenes/Sleeve.gd
@@ -1,4 +1,4 @@
-extends CSGBox
+extends MeshInstance
 
 const DISPLACEMENT = 0.25
 
@@ -12,3 +12,9 @@ func reveal():
 		d = -d
 	tween.tween_property(self, "translation", Vector3(translation.x, translation.y, translation.z + d), 0.3).from_current()
 	revealed = !revealed
+
+
+func set_textures(side_a, side_b, edge_color):
+	mesh.surface_get_material(0).set_shader_param("side_a", side_a)
+	mesh.surface_get_material(0).set_shader_param("side_b", side_b)
+	mesh.surface_get_material(0).set_shader_param("edge_color", edge_color)
